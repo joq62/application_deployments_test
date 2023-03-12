76dc7fd687f3519d2db9b92c07098a92bd33a068
joq62 <jakim.leche@gmail.com>
2023-03-12 23:08:35 +0100

    created

---
+++ application_deployment_check.erl
@@ -0,0 +1,51 @@
+-module(application_deployment_check).
+
+-export([
+	 start/0,
+	 %% Support
+	 all_files/0,
+	 all_info/0
+	]).
+
+
+-define(Dir,".").
+-define(FileExt,".deployment").
+
+%% --------------------------------------------------------------------
+%% Function: available_hosts()
+%% Description: Based on hosts.config file checks which hosts are avaible
+%% Returns: List({HostId,Ip,SshPort,Uid,Pwd}
+%% --------------------------------------------------------------------
+start()->
+    
+    check(all_info()),
+    init:stop(),
+    ok.
+
+check([])->
+    io:format("Ok, no errors!! ~n");
+check([{ok,[{appl_deployment,_Id,Info}]}|T])->
+    io:format("Checking ~p~n",[Info]),
+    true=proplists:is_defined(appl_spec,Info),
+    true=proplists:is_defined(vsn,Info),
+    true=proplists:is_defined(cluster_spec,Info),
+    true=proplists:is_defined(num_instances,Info),
+    true=proplists:is_defined(affinity,Info),
+    check(T).
+
+   
+
+%% --------------------------------------------------------------------
+%% Function: available_hosts()
+%% Description: Based on hosts.config file checks which hosts are avaible
+%% Returns: List({HostId,Ip,SshPort,Uid,Pwd}
+%% --------------------------------------------------------------------
+all_files()->
+    {ok,Files}=file:list_dir(?Dir),
+    FileNames=[filename:join(?Dir,Filename)||Filename<-Files,
+					     ?FileExt=:=filename:extension(Filename)],
+    FileNames.    
+all_info()->
+    [file:consult(File)||File<-all_files()].
+	    
+    
